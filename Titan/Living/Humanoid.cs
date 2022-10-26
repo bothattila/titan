ef952d8f2eac9088180c9e42b936ccf83779d049
botyi <botyi@DESKTOP-5C7QS0G>
2022-10-26 18:44:39 +0200

    Add project files.

---
+++ Titan/Living/Humanoid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Titan.Living
+{
+    internal class Humanoid
+    {
+        public string Name;
+
+        public void Greeting()
+        {
+            Console.WriteLine("Greetings, I am a humanoid");
+        }
+
+        public void Introducing()
+        {
+            Console.WriteLine("My name is " + Name);
+        }
+
+
+
+
+    }
+}
---
+++ Titan/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Titan.Living;
+
+namespace Titan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Humanoid humanoid = new Humanoid();
+            humanoid.Name = "Attila";
+            humanoid.Greeting();
+            humanoid.Introducing();
+            
+
+            // Console.WriteLine("Hello");
+            Console.ReadKey();
+        }
+    }
+}
